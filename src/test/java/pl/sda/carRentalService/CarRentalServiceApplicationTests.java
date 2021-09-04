02cd29120921f67ec7238a3fd22668dcec3b62e8
uzytkownik <vova.prantsan2@gmail.com>
2021-09-04 21:36:17 +0200

    first commit

---
+++ src/test/java/pl/sda/carRentalService/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.carRentalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
